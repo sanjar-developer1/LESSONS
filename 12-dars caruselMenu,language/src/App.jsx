17ebe10b74e114c925881351d6560e4a7be521bd
sanjarcik <sanjarbekarabboyev22@gmail.com>
2025-07-17 15:50:11 +0500

    added

---
+++ 12-dars caruselMenu,language/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import CaruselMenu from "./CaruselMenu";
+
+function App() {
+  return (
+    <>
+      <h1 style={{ textAlign: "center" }}>CaruselMenu birinchisi</h1>
+      <CaruselMenu />
+    </>
+  );
+}
+
+export default App;
